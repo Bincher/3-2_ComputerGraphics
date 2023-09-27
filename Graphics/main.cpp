4f6a0b2075200bce8caf39d2d21a8af1e3e90926
seongbin <catsb21@gmail.com>
2023-09-27 13:06:22 +0900

    과제1 - Bresenham

--- Graphics/main.cpp
+++ Graphics/main.cpp
@@ -8,63 +8,110 @@
 #define W 640
 
 //unsigned char Iarray[H][W];
-unsigned char Rarray[H][W];
-unsigned char Garray[H][W];
-unsigned char Barray[H][W];
-int linepatt[4][8] = { 1,1,1,1,1,1,1,1,//SOLID
-						1,1,1,1,0,0,0,0,//DASHED
-						1,0,1,0,1,0,1,0,//DOTTED
-						1,1,1,0,1,1,1,0 };//User defined
+unsigned char Iarray[H][W][3];
 
-enum { SOLID, DASHED, DOTTED, USER };
-
-void setPixel(int x, int y, unsigned char r, unsigned char g, unsigned char b) {
-    Rarray[x][y] = r;
-    Garray[x][y] = g;
-    Barray[x][y] = b;
-}
-
-void lineDDA(int x1, int y1, int x2, int y2, int lt, unsigned char r, unsigned char g, unsigned char b)
+void lineBres(int x1, int y1, int x2, int y2, unsigned char r, unsigned char g, unsigned char b)
 {
-    int dx = x2 - x1, dy = y2 - y1, step, k;
-    float xinc, yinc, x = (float)x1, y = (float)y1;
+    int dx = abs(x2 - x1), dy = abs(y2 - y1);
+    int p, twoDy, twoDyDx;
+    int x, y, xEnd, yEnd, IorD;
 
-    if (abs(dx) > abs(dy)) step = abs(dx);
-    else step = abs(dy);
-    xinc = dx / (float)step;
-    yinc = dy / (float)step;
+    if (dx > dy) {
+        p = 2 * dy - dx; twoDy = 2 * dy; twoDyDx = 2 * (dy - dx);
 
-    setPixel(x1, y1, r, g, b); // start pixel marking(setPixel function)
-    for (k = 0; k < step; k++) {
-        x += xinc;
-        y += yinc;
-        if (linepatt[lt][k % 8])
-            setPixel((int)(x + 0.5), (int)(y + 0.5), r, g, b); // marking(setPixel function)
-    }
+        if (x1 > x2) { 
+            x = x2; 
+            y = y2; 
+            xEnd = x1; 
+            if (y1 - y2 > 0)IorD = 1; 
+            else IorD = -1; 
+        }
+        else { 
+            x = x1; 
+            y = y1; 
+            xEnd = x2; 
+            if (y2 - y1 > 0)IorD = 1; 
+            else IorD = -1; 
+        }
+
+        Iarray[x][y][0] = r; 
+        Iarray[x][y][1] = g; 
+        Iarray[x][y][2] = b;// start point marking with a user-defined color(r,g,b) 
+
+        while (x < xEnd) {
+            x++;
+            if (p < 0) p += twoDy;
+            else { 
+                y += IorD; 
+                p += twoDyDx; 
+            }
+            Iarray[x][y][0] = r; 
+            Iarray[x][y][1] = g; 
+            Iarray[x][y][2] = b;//marking
+        }// end of while
+    }// end of if
+    else {
+        p = 2 * dx - dy;
+        twoDy = 2 * dx;
+        twoDyDx = 2 * (dx - dy);
+
+        if (y1 > y2) {
+            x = x2;
+            y = y2;
+            yEnd = y1;
+            if (x1 - x2 > 0) IorD = 1;
+            else IorD = -1;
+        }
+        else {
+            x = x1;
+            y = y1;
+            yEnd = y2;
+            if (x2 - x1 > 0) IorD = 1;
+            else IorD = -1;
+        }
+
+        Iarray[x][y][0] = r;
+        Iarray[x][y][1] = g;
+        Iarray[x][y][2] = b; // 시작점 표시
+
+        while (y < yEnd) {
+            y++;
+            if (p < 0) p += twoDy;
+            else {
+                x += IorD;
+                p += twoDyDx;
+            }
+            Iarray[x][y][0] = r;
+            Iarray[x][y][1] = g;
+            Iarray[x][y][2] = b; // 해당 픽셀 색칠
+        }
+    }// end of else
+
 }
+
 
 void main()
 {
-    memset(Rarray, 255, H * W);
-    memset(Garray, 255, H * W);
-    memset(Barray, 255, H * W);
+    memset(Iarray, 255, H * W);
+    memset(Iarray, 255, H * W);
+    memset(Iarray, 255, H * W);
 
-    lineDDA(0, 0, H - 1, W - 1, SOLID, 255, 0, 0); // Red SOLID line
-    lineDDA(H - 1, 0, 0, W - 1, DASHED, 0, 255, 0); // Green DASHED line
-    lineDDA(10, 20, 300, 200, DOTTED, 0, 0, 255); // Blue DOTTED line
-    lineDDA(200, 100, 0, 500, USER, 255, 255, 0); // Yellow USER line
+    lineBres(0, 0, H - 1, W - 1, 255, 0, 0);
+    lineBres(H - 1, 0, 0, W - 1, 0, 255, 0);
+    lineBres(10, 20, 300, 200, 0, 0, 255);
+    lineBres(200, 100, 0, 500, 255, 255, 0);
 
     FILE* fp;
-    fp = fopen("dda-lp.ppm", "wb");
+    fp = fopen("bres-lp.ppm", "wb");
     fprintf(fp, "P6\n");
     fprintf(fp, "%d %d\n", W, H); // file size
     fprintf(fp, "255\n"); // max level
 
     for (int i = 0; i < H; i++) {
         for (int j = 0; j < W; j++) {
-            fwrite(&Rarray[i][j], sizeof(unsigned char), 1, fp);
-            fwrite(&Garray[i][j], sizeof(unsigned char), 1, fp);
-            fwrite(&Barray[i][j], sizeof(unsigned char), 1, fp);
+            fwrite(&Iarray[i][j], sizeof(unsigned char), 1, fp);
+            fwrite(&Iarray[i][j], sizeof(unsigned char), 1, fp);
+            fwrite(&Iarray[i][j], sizeof(unsigned char), 1, fp);
         }
     }
 
