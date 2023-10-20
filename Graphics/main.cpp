268e43c7acb749e717485b3f639d493a6c22283f
seongbin <catsb21@gmail.com>
2023-10-20 14:45:58 +0900

    과제 2 - koch curve

--- Graphics/main.cpp
+++ Graphics/main.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-#pragma warning(disable: 4996) 
+#pragma warning(disable: 4996)
 #define H 680
 #define W 840
 
@@ -9,11 +9,11 @@ unsigned char Iarray[H][W][3];
 int linepatt[4][8] = { 1,1,1,1,1,1,1,1, // SOLID
                       1,1,1,1,0,0,0,0, // DASHED
                       1,0,1,0,1,0,1,0, // DOTTED
-                      1,1,1,0,1,1,1,0 }; // USER
+                      1,1,1,0,1,1,1,0 }; // USER;
 
 enum { SOLID, DASHED, DOTTED, USER };
 
-// DDA
+// DDA 알고리즘을 이용한 선 그리기 함수
 void lineDDA(int x1, int y1, int x2, int y2, int lt, unsigned char r, unsigned char g, unsigned char b) {
     int dx = x2 - x1, dy = y2 - y1, step, k;
     float xinc, yinc, x = (float)x1, y = (float)y1;
@@ -23,83 +23,59 @@ void lineDDA(int x1, int y1, int x2, int y2, int lt, unsigned char r, unsigned c
     xinc = dx / (float)step;
     yinc = dy / (float)step;
 
-    Iarray[x1][y1][0] = r; // setpixel
+    Iarray[x1][y1][0] = r; // 시작 픽셀 마킹 (setpixel 함수)
     Iarray[x1][y1][1] = g;
     Iarray[x1][y1][2] = b;
     for (k = 0; k < step; k++) {
         x += xinc;
         y += yinc;
         if (linepatt[lt][k % 8]) {
-            Iarray[(int)(x + 0.5)][(int)(y + 0.5)][0] = r; // 마킹
+            Iarray[(int)(x + 0.5)][(int)(y + 0.5)][0] = r; // 마킹 (setpixel 함수)
             Iarray[(int)(x + 0.5)][(int)(y + 0.5)][1] = g;
             Iarray[(int)(x + 0.5)][(int)(y + 0.5)][2] = b;
         }
     }
 }
 
-// 베지어 스플라인
-void drawBezierSpline(int x[], int y[], int n, int r, int g, int b) {
-    int C[6] = { 1, 5, 10, 10, 5, 1 };
-    float t, u, xpt, ypt;
-    int npts, k, j;
-    int* bin;
-
-    npts = 500;
-
-    bin = new int[n + 1];
-
-    // 이항 계수 C
-    for (k = 0; k <= n; k++) {
-        bin[k] = 1;
-        for (j = n; j >= k + 1; j--)
-            bin[k] *= j;
-        for (j = n - k; j >= 2; j--)
-            bin[k] /= j;
+// Koch 곡선 그리기 함수
+void kochCurve(int x1, int y1, int x5, int y5, int depth, int lt, unsigned char r, unsigned char g, unsigned char b) {
+    int x2, y2, x3, y3, x4, y4;
+    int dx, dy;
+    if (depth == 0) {
+        lineDDA(x1, y1, x5, y5, lt, r, g, b);
     }
+    else {
 
-    // 그리기
-    for (k = 0; k < npts; k++) {
-        t = (float)k / (npts - 1);
-        u = 1 - t;
-        xpt = 0;
-        ypt = 0;
+        dx = (x5 - x1) / 3;
+        dy = (y5 - y1) / 3;
+        x2 = x1 + dx;
+        y2 = y1 + dy;
+        x3 = (int)(0.5 * (x1 + x5) + sqrt(3) * (y1 - y5) / 6);
+        y3 = (int)(0.5 * (y1 + y5) + sqrt(3) * (x5 - x1) / 6);
+        x4 = 2 * dx + x1;
+        y4 = 2 * dy + y1;
 
-        for (j = 0; j <= n; j++) {
-            xpt += bin[j] * pow(u, (float)(n - j)) * pow(t, (float)j) * x[j];
-            ypt += bin[j] * pow(u, (float)(n - j)) * pow(t, (float)j) * y[j];
-        }
-
-        if (k > 0) {
-            lineDDA((int)xpt, (int)ypt, (int)xpt, (int)ypt, SOLID, r, g, b);
-        }
+        kochCurve(x1, y1, x2, y2, depth - 1, lt, r, g, b);
+        kochCurve(x2, y2, x3, y3, depth - 1, lt, r, g, b);
+        kochCurve(x3, y3, x4, y4, depth - 1, lt, r, g, b);
+        kochCurve(x4, y4, x5, y5, depth - 1, lt, r, g, b);
     }
-
-    delete[] bin;
-    printf("그리기 종료\n");
 }
 
 int main() {
     printf("시작\n");
-
-    int numControlPoints = 5;
-    int x[5] = { 50, 150, 250, 350, 450 };
-    int y[5] = { 150, 450, 50, 400, 100 };
-
-    drawBezierSpline(x, y, numControlPoints - 1, 255, 255, 255);
-
+    kochCurve(280, 280, 150, 20, 5, SOLID, 255, 255, 255);
+    kochCurve(150 , 20, 20, 280, 5, SOLID, 255, 255, 255);
+    kochCurve(20, 280, 280, 280, 5, SOLID, 255, 255, 255);
+    
+   
     printf("파일 생성 시작\n");
     FILE* fp;
-    fp = fopen("bezier_spline.ppm", "wb");
+    fp = fopen("koch_curve.ppm", "wb");
     fprintf(fp, "P6\n");
     fprintf(fp, "%d %d\n", W, H); // 파일 크기
     fprintf(fp, "255\n");         // 최대 레벨
-    for (int i = 0; i < numControlPoints - 1; i++) {
-        int x1 = x[i];
-        int y1 = y[i];
-        int x2 = x[i+1];
-        int y2 = y[i+1];
-        lineDDA(x1, y1, x2, y2, DASHED, 255, 255, 255);
-    }
+
     for (int i = 0; i < H; i++) {
         for (int j = 0; j < W; j++) {
             fwrite(&Iarray[i][j], sizeof(unsigned char), 3, fp);
