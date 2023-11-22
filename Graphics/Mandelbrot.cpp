9b500ccceae4053b05f7de39d55dc7af5dc82f7a
seongbin <catsb21@gmail.com>
2023-11-22 12:07:07 +0900

    망델브로 드래그문제 해결중,,,

--- Graphics/Mandelbrot.cpp
+++ Graphics/Mandelbrot.cpp
@@ -9,11 +9,11 @@ int mFlag = 0;
 
 GLint TopLeftX, TopLeftY, BottomRightX, BottomRightY;
 
-void generateMandelbrot() {
+void generateMandelbrot(double xMin, double xMax, double yMin, double yMax) {
     for (int i = 0; i < M; i++) {
         for (int j = 0; j < M; j++) {
-            double x = (double)j / M * 3.5 - 2.5;
-            double y = (double)i / M * 2.0 - 1.0;
+            double x = xMin + (double)j / M * (xMax - xMin);
+            double y = yMin + (double)i / M * (yMax - yMin);
 
             double zx = 0, zy = 0;
             double cx = x, cy = y;
@@ -32,16 +32,19 @@ void generateMandelbrot() {
     }
 }
 
+void generateMandelbrotForViewport() {
+    double xMin = (double)TopLeftX / M * 3.5 - 2.5;
+    double xMax = (double)BottomRightX / M * 3.5 - 2.5;
+    double yMin = (double)TopLeftY / M * 2.0 - 1.0;
+    double yMax = (double)BottomRightY / M * 2.0 - 1.0;
+    generateMandelbrot(xMin, xMax, yMin, yMax);
+}
+
 void MyDisplay() {
     glViewport(0, 0, 500, 500);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // Check if the flag is not set (initial display) or if dragging is not in progress
-    if (!mFlag) {
-        generateMandelbrot();
-    }
-
-    // Mandelbrot set or zoomed region display
+    // Draw the Mandelbrot
     glBegin(GL_POINTS);
     for (int i = 0; i < M; i++) {
         for (int j = 0; j < M; j++) {
@@ -66,19 +69,25 @@ void MyDisplay() {
 }
 
 void MyMouseClick(GLint Button, GLint State, GLint X, GLint Y) {
-    if (Button == GLUT_LEFT_BUTTON && State == GLUT_DOWN) {
-        TopLeftX = X;
-        TopLeftY = Y;
-        mFlag = 1;
-        printf("시작 좌표 : %d %d\n", X, Y);
-    }
-    if (Button == GLUT_LEFT_BUTTON && State == GLUT_UP) {
-        mFlag = 0;
-        printf("끝 좌표 : %d %d\n", X, Y);
-        printf("%d %d %d %d\n", TopLeftX, TopLeftY, BottomRightX, BottomRightY);
-        
-    }
+    if (Button == GLUT_LEFT_BUTTON) {
+        if (State == GLUT_DOWN) {
+            TopLeftX = X;
+            TopLeftY = Y;
+            mFlag = 1;
+            printf("시작 좌표 : %d %d\n", X, Y);
+        }
+        else if (State == GLUT_UP) {
+            BottomRightX = X;
+            BottomRightY = Y;
+            mFlag = 0;
 
+            // Generate Mandelbrot based on the selected region
+            generateMandelbrotForViewport();
+
+            printf("끝 좌표 : %d %d\n", X, Y);
+            glutPostRedisplay();
+        }
+    }
 }
 
 void MyMouseMove(GLint X, GLint Y) {
@@ -96,7 +105,7 @@ void MyInit() {
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);  // 더블 버퍼 사용을 위해 GLUT_DOUBLE 추가
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
     glutInitWindowSize(500, 500);
     glutInitWindowPosition(0, 0);
     glutCreateWindow("Mandelbrot Explorer");
@@ -105,8 +114,8 @@ int main(int argc, char** argv) {
     glutMouseFunc(MyMouseClick);
     glutMotionFunc(MyMouseMove);
 
-    // 초기에 Mandelbrot 집합을 생성하여 화면에 출력
-    generateMandelbrot();
+    // Initial display of the Mandelbrot set
+    generateMandelbrot(-2.5, 1.0, -1.0, 1.0);
     glutPostRedisplay();
 
     glutMainLoop();
